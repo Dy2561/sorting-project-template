12791b584051a811998d72a37d257a096735b66b
Dy2561 <dylanyun2009@gmail.com>
2025-01-26 07:30:35 +0000

    Finished

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,6 +1,19 @@
 public class SelectionSort implements Sorter {
 
+    @Override
     public void sort(int[] input) {
-        System.out.println("Selection Sort!!!");
+        System.out.println("Selection sort: ");
+
+        for (int i = 0; i < input.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < input.length; j++) {
+                if (input[j] < input[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = input[minIndex];
+            input[minIndex] = input[i];
+            input[i] = temp;
+        }
     }
 }
